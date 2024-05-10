844ae98d6436161f0e34ad078372689f23dc38e5
azmiyushari <azmiyushrr@gmail.com>
2024-05-10 23:56:21 +0700

    Menghapus folder hw77

--- hw7/modul.js
+++
@@ -1,9 +0,0 @@
-exports.luasPersegi = sisi => {return sisi * sisi};
-
-exports.kelilingPersegi = sisi => {return 4 * sisi};
-
-exports.luasPersegiPanjang = (panjang, lebar) => {return panjang * lebar};
-
-exports.kelilingPersegiPanjang = (panjang, lebar) => {
-    return 2 * (panjang + lebar)
-};
